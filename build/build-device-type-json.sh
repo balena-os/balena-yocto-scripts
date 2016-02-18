ff00b2e8405852fad3adf37b8960aee7c2b99bf0
Florin Sarbu <florin@resin.io>
2016-02-18 06:50:30 +0200

    Check for existence of npm, node and jq
    We need to display error messages when these commands are not available
    and instruct the user to install them.
    
    Signed-off-by: Florin Sarbu <florin@resin.io>

--- build/build-device-type-json.sh
+++ build/build-device-type-json.sh
@@ -8,12 +8,18 @@ mydir=`dirname $0`
 
 cd $mydir/../../
 
+function quit {
+    rm -f "$slug".json
+    echo $0: $1 >&2
+    exit 1
+}
+
 for filename in *.coffee; do
     slug="${filename%.*}"
-    npm install coffee-script --production
+    npm install coffee-script --production 2>/dev/null || quit "ERROR - Please install the 'npm' package before running this script."
     cd ./resin-device-types && npm install --production && cd ..
 
-NODE_PATH=. node > "$slug".json << EOF
+{ NODE_PATH=. node > "$slug".json 2>/dev/null << EOF
     require('coffee-script/register');
     var dt = require('resin-device-types');
     var manifest = require('./${filename}');
@@ -21,4 +27,10 @@ NODE_PATH=. node > "$slug".json << EOF
     var builtManifest = dt.buildManifest(manifest, slug);
     console.log(JSON.stringify(builtManifest, null, '\t'));
 EOF
+} || quit "ERROR - Please install the 'nodejs' package before running this script."
+
+if [ ! -s "$slug".json ]; then
+    quit "ERROR - Please check your nodejs installation. The 'node' binary did not generate proper .json(s)."
+fi
+
 done
