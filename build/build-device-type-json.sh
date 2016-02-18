669e752bf06f13ae44492896fecc98f7bda2ebb1
Florin Sarbu <florin@resin.io>
2016-02-18 06:50:30 +0200

    Check for existence of npm, node and jq
    We need to display error messages when these commands are not available
    and instruct the user to install them.
    
    Signed-off-by: Florin Sarbu <florin@resin.io>

--- build/build-device-type-json.sh
+++ build/build-device-type-json.sh
@@ -10,10 +10,10 @@ cd $mydir/../../
 
 for filename in *.coffee; do
     slug="${filename%.*}"
-    npm install coffee-script --production
+    npm install coffee-script --production 2>/dev/null || { echo "Error - Please install the 'npm' package before running this script." >&2; exit 1; }
     cd ./resin-device-types && npm install --production && cd ..
 
-NODE_PATH=. node > "$slug".json << EOF
+{ NODE_PATH=. node > "$slug".json 2>/dev/null << EOF
     require('coffee-script/register');
     var dt = require('resin-device-types');
     var manifest = require('./${filename}');
@@ -21,4 +21,5 @@ NODE_PATH=. node > "$slug".json << EOF
     var builtManifest = dt.buildManifest(manifest, slug);
     console.log(JSON.stringify(builtManifest, null, '\t'));
 EOF
+} || { echo "Error - Please install the 'nodejs' package before running this script." >&2; rm "$slug".json; exit 1; }
 done
