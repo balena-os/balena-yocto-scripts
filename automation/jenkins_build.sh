28690eef3f64fbb4bb43e31cdcd7e5a2fc6fd6d1
Andrei Gherzan <andrei@resin.io>
2017-04-12 17:24:13 +0100

    jenkins_build.sh: Use S3_CMD after required package is installed
    Signed-off-by: Andrei Gherzan <andrei@resin.io>

--- automation/jenkins_build.sh
+++ automation/jenkins_build.sh
@@ -216,9 +216,9 @@ if [ "$deploy" == "yes" ]; then
 		-v $S3_DEPLOY_DIR:/host/images resin/resin-img:master /bin/sh -x -c ' \
 		echo "${BUILD_VERSION}" > "/host/images/${SLUG}/latest" \
 		&& /usr/src/app/node_modules/.bin/coffee /usr/src/app/scripts/prepare.coffee \
-		&& ([ "${DEVELOPMENT_IMAGE}" = "no" ] && $S3_CMD put /host/images/${SLUG}/latest s3://${S3_BUCKET}/${SLUG}/ || true) \
 		&& apt-get -y update \
 		&& apt-get install -y s3cmd \
+		&& ([ "${DEVELOPMENT_IMAGE}" = "no" ] && $S3_CMD put /host/images/${SLUG}/latest s3://${S3_BUCKET}/${SLUG}/ || true) \
 		&& ([ -z "$($S3_CMD ls s3://${S3_BUCKET}/${SLUG}/${BUILD_VERSION}/)" ] \
 		&& $S3_CMD $S3_SYNC_OPTS sync /host/images/${SLUG}/${BUILD_VERSION}/ s3://${S3_BUCKET}/${SLUG}/${BUILD_VERSION}/)'
 fi
