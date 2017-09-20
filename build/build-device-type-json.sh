162d603654b26dadba4d6e5228ef485b594d9fa5
Will Newton <willn@resin.io>
2017-09-20 19:23:56 +0300

    build-device-type-json.sh: Make npm failures louder
    Currently if npm fails it is difficult to debug.
    
    Signed-off-by: Will Newton <willn@resin.io>

--- build/build-device-type-json.sh
+++ build/build-device-type-json.sh
@@ -17,7 +17,7 @@ function quit {
     exit 1
 }
 
-npm install --production --silent >/dev/null || quit "ERROR - Please install the 'npm' package before running this script."
+npm install --production --quiet || quit "ERROR - Please make sure the 'npm' package is installed and working before running this script."
 
 which nodejs >/dev/null 2>&1 && NODE=nodejs || NODE=node
 
