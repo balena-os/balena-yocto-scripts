97edc19568694f6f93a3d242dd3c7feeded90ae6
Giovanni Garufi <giovanni@balena.io>
2019-10-08 13:09:21 +0200

    Add ESR-line parameter
    Change-type: minor
    Signed-off-by: Giovanni Garufi <giovanni@balena.io>

--- automation/start-docker-and-push.sh
+++ automation/start-docker-and-push.sh
@@ -20,6 +20,18 @@ else
 	balena login --token $BALENAOS_PRODUCTION_TOKEN
 fi
 
+case $ESR_LINE in
+	next|current|sunset)
+		SLUG="${SLUG}-esr"
+		;;
+	__ignore__)
+		;;
+	*)
+		echo "Invalid ESR line"
+		exit 1
+		;;
+esac
+
 echo "[INFO] Pushing $_local_image to balenaos/$SLUG"
 
 _releaseID=$(balena deploy "balenaos/$SLUG" "$_local_image" | sed -n 's/.*Release: //p')
@@ -32,6 +44,9 @@ fi
 balena tag set version $VERSION_HOSTOS --release $_releaseID
 balena tag set variant $_variant --release $_releaseID
 balena tag set status "Untested" --release $_releaseID
+if [ "$ESR_LINE" != "__ignore__" ]; then
+	balena tag set "ESR-line" $ESR_LINE --release $_releaseID
+fi
 
 cleanup
 exit 0
