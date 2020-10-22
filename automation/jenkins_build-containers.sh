a55c851a168cb91b30104527584c4f74e8c5b9e8
Alex Gonzalez <alexg@balena.io>
2020-10-22 10:42:17 +0200

    jenkins_build-containers: Add support to build device containers
    This allows to use balena CLI to build containers for device
    architectures.
    
    Change-type: patch
    Changelog-entry: Helper container images can now be device specific
    Signed-off-by: Alex Gonzalez <alexg@balena.io>

--- automation/jenkins_build-containers.sh
+++ automation/jenkins_build-containers.sh
@@ -2,16 +2,11 @@
 
 set -ev
 
-DOCKERFILES=( Dockerfile_yocto-build-env Dockerfile_balena-push-env )
+[ -z "${DOCKERFILES}" ] && DOCKERFILES=( Dockerfile_package-based-hostext.template Dockerfile_yocto-build-env Dockerfile_balena-push-env )
 
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 REVISION=$(cd "${script_dir}" && git rev-parse --short HEAD)
 NAMESPACE=${NAMESPACE:-resin}
-
-# Get the absolute script location
-pushd `dirname $0` > /dev/null 2>&1
-SCRIPTPATH=`pwd`
-popd > /dev/null 2>&1
 
 if [ -z "${JOB_NAME}" ]; then
     echo "[ERROR] No job name specified."
@@ -20,14 +15,38 @@ fi
 
 for DOCKERFILE in "${DOCKERFILES[@]}"
 do
-  REPO_NAME=${DOCKERFILE#"Dockerfile_"}
+  REPO_NAME=${DOCKERFILE%".template"}
+  REPO_NAME=${REPO_NAME#"Dockerfile_"}
+  BUILDER="docker"
+  BUILDER_OPTS=""
+  REPO_NAME="${REPO_NAME}"
+  case ${DOCKERFILE} in
+    *template)
+      BUILDER="balena"
+      [ -z "${DEVICE_TYPE}" ] && echo "Device type is required for ${BUILDER} builder." && exit 1
+      [ -z "${DEVICE_ARCH}" ] && echo "Device architecture is required for ${BUILDER} builder." && exit 1
+      BUILDER_OPTS="--deviceType ${DEVICE_TYPE:?"Device type is required"} --arch ${DEVICE_ARCH:?"Device architecture is required"} --buildArg NAMESPACE=${NAMESPACE}"
+      DOCKERFILE_PATH="--dockerfile ${DOCKERFILE}"
+      NOCACHE="--nocache"
+      REPO_NAME="${DEVICE_TYPE}-${REPO_NAME}"
+      TAG="--projectName ${NAMESPACE}/${REPO_NAME}"
+      ;;
+    *)
+      NOCACHE="--no-cache"
+      TAG="--tag ${NAMESPACE}/${REPO_NAME}:${REVISION}"
+      DOCKERFILE_PATH="-f ${script_dir}/${DOCKERFILE}"
+      BUILDER_OPTS="--build-arg NAMESPACE=${NAMESPACE}"
+      ;;
+  esac
   # Build
-  docker build --pull --no-cache --tag ${NAMESPACE}/${REPO_NAME}:${REVISION} -f ${SCRIPTPATH}/${DOCKERFILE} ${SCRIPTPATH}
+  "${BUILDER}" build --pull ${NOCACHE} ${BUILDER_OPTS} ${TAG} ${DOCKERFILE_PATH} "${script_dir}"
 
-  # Tag
-  docker tag ${NAMESPACE}/${REPO_NAME}:${REVISION} ${NAMESPACE}/${REPO_NAME}:latest
+  if [ "${BUILDER}" = "balena" ]; then
+      docker tag "${NAMESPACE}/${REPO_NAME}_main" "${NAMESPACE}"/"${REPO_NAME}":"${REVISION}"
+  fi
+  docker tag "${NAMESPACE}"/"${REPO_NAME}":"${REVISION}" "${NAMESPACE}"/"${REPO_NAME}":latest
 
   # Push
-  docker push ${NAMESPACE}/${REPO_NAME}:${REVISION}
-  docker push ${NAMESPACE}/${REPO_NAME}:latest
+  docker push "${NAMESPACE}"/"${REPO_NAME}":"${REVISION}"
+  docker push "${NAMESPACE}"/"${REPO_NAME}":latest
 done
