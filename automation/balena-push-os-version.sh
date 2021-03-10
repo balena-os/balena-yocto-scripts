8f97998313974565b2a837988c376806521b2558
Alex Gonzalez <alexg@balena.io>
2021-03-10 12:59:27 +0100

    balena-push-os-version: Set the release version on deploy
    Until "balena deploy" has this functionality let's perform a direct
    API call.
    
    Production versions have the OS release name and development versions get
    appended a .dev suffix. Once both image variants are merged only the OS
    release name will be used.
    
    Change-type: patch
    Signed-off-by: Alex Gonzalez <alexg@balena.io>

--- automation/balena-push-os-version.sh
+++ automation/balena-push-os-version.sh
@@ -13,13 +13,9 @@ wait_docker
 _local_image=$(docker load -i /host/appimage.docker | cut -d: -f1 --complement | tr -d " " )
 BALENAOS_ACCOUNT="balena_os"
 
-echo "[INFO] Logging into $DEPLOY_TO as ${BALENAOS_ACCOUNT}"
-if [ "$DEPLOY_TO" = "staging" ]; then
-	export BALENARC_BALENA_URL=balena-staging.com
-	balena login --token $BALENAOS_STAGING_TOKEN
-else
-	balena login --token $BALENAOS_PRODUCTION_TOKEN
-fi
+echo "[INFO] Logging into $API_ENV as ${BALENAOS_ACCOUNT}"
+export BALENARC_BALENA_URL=${API_ENV}
+balena login --token "${TOKEN}"
 
 _app_suffix=""
 if [ "$ESR" = "true" ]; then
@@ -29,18 +25,21 @@ fi
 echo "Is this an ESR version? ${ESR}"
 echo "[INFO] Pushing $_local_image to ${BALENAOS_ACCOUNT}/$APPNAME$_app_suffix"
 _releaseID=$(balena deploy "${BALENAOS_ACCOUNT}/$APPNAME$_app_suffix" "$_local_image" | sed -n 's/.*Release: //p')
-if [ "$DEVELOPMENT_IMAGE" = "yes" ]; then
-	_variant="development"
+if [ "${_variant}" = "dev" ]; then
+	release_version="${VERSION_HOSTOS}.dev"
+	variant_str="development"
 else
-	_variant="production"
+	release_version="${VERSION_HOSTOS}"
+	variant_str="production"
 fi
 
-echo "[INFO] Tagging release ${_releaseID} with version ${VERSION_HOSTOS} and variant ${_variant}"
+echo "[INFO] Tagging release ${_releaseID} with version ${release_version} and variant ${variant_str}"
 balena tag set version $VERSION_HOSTOS --release $_releaseID
-balena tag set variant $_variant --release $_releaseID
+balena tag set variant $variant_str --release $_releaseID
 if [ "$ESR" = "true" ]; then
 	balena tag set meta-balena-base $META_BALENA_VERSION --release $_releaseID
 fi
+balena_api_set_release_version "${_releaseID}" "${BALENARC_BALENA_URL}" "${TOKEN}" "${release_version}"
 
 cleanup
 exit 0
