00bacf68d287bcab171d2872b05b006c06d93fe1
Alex Gonzalez <alexg@balena.io>
2021-04-19 13:07:25 +0200

    [TEST] Revert using newer docker in yocto-build-env

--- automation/entry_scripts/prepare-and-start.sh
+++ automation/entry_scripts/prepare-and-start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-source /balena-docker.inc
+source /manage-docker.sh
 
-trap 'balena_docker_stop fail' SIGINT SIGTERM
+trap 'cleanup fail' SIGINT SIGTERM
 
 INSTALL_DIR="/work"
 
 # Create the normal user to be used for bitbake (barys)
 echo "[INFO] Creating and setting builder user $BUILDER_UID:$BUILDER_GID."
 groupadd -g $BUILDER_GID builder
-if ! cat "/etc/group" | grep docker > /dev/null; then  groupadd docker; fi
-useradd -m -u $BUILDER_UID -g $BUILDER_GID -G docker builder && newgrp docker
+groupadd docker
+useradd -m -u $BUILDER_UID -g $BUILDER_GID -G docker builder
 
 # Make the "builder" user inherit the $SSH_AUTH_SOCK variable set-up so he can use the host ssh keys for various operations
 # (like being able to clone private git repos from within bitbake using the ssh protocol)
@@ -23,8 +23,9 @@ mkdir -p /home/builder/.ssh/
 echo "StrictHostKeyChecking no" > /home/builder/.ssh/config
 
 # Start docker
-balena_docker_start
-balena_docker_wait
+echo "[INFO] Starting docker."
+docker daemon 2> /dev/null &
+wait_docker
 
 # Authenticate with Balena registry if required
 BALENAOS_ACCOUNT="balena_os"
@@ -54,5 +55,5 @@ fi
 barys_pid=$!
 wait $barys_pid || true
 
-balena_docker_stop
+cleanup
 exit 0
