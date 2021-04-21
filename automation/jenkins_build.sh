74a5bcaa4761f1f0bdf7ea8fba153540d33e2313
Alex Gonzalez <alexg@balena.io>
2021-04-21 19:52:24 +0200

    jenkins_build: Replace supervisor tag with supervisor release
    The latest meta-balena fetches the supervisor from the internal registry
    using the release version.
    
    Change-type: patch
    Signed-off-by: Alex Gonzalez <alexg@balena.io>

--- automation/jenkins_build.sh
+++ automation/jenkins_build.sh
@@ -18,8 +18,8 @@ print_help() {
 	\t\t --meta-balena-branch\n\
 	\t\t\t (optional) The meta-balena branch to checkout before building.\n\
 \t\t\t\t Default value is __ignore__ which means it builds the meta-balena revision as configured in the git submodule.\n
-	\t\t --supervisor-tag\n\
-	\t\t\t (optional) The resin supervisor tag specifying which supervisor version is to be included in the build.\n\
+	\t\t --supervisor-version\n\
+	\t\t\t (optional) The balena supervisor release version to be included in the build.\n\
 \t\t\t\t Default value is __ignore__ which means use the supervisor version already included in the meta-balena submodule.\n
 	\t\t --preserve-build\n\
 	\t\t\t (optional) Do not delete existing build directory.\n\
@@ -93,12 +93,12 @@ while [[ $# -ge 1 ]]; do
 			fi
 			metaResinBranch="${metaResinBranch:-$2}"
 			;;
-		--supervisor-tag)
+		--supervisor-version)
 			if [ -z "$2" ]; then
-				echo "--supervisor-tag argument needs a resin supervisor tag name (if this option is not used, the default value is __ignore__)"
+				echo "--supervisor-version argument needs a balena supervisor release version (if this option is not used, the default value is __ignore__)"
 				exit 1
 			fi
-			supervisorTag="${supervisorTag:-$2}"
+			supervisorVersion="${supervisorVersion:-$2}"
 			;;
 		--esr)
 			ESR="true"
@@ -115,7 +115,7 @@ while [[ $# -ge 1 ]]; do
 done
 
 metaResinBranch=${metaResinBranch:-__ignore__}
-supervisorTag=${supervisorTag:-__ignore__}
+supervisorVersion=${supervisorVersion:-__ignore__}
 
 # Sanity checks
 if [ -z "$MACHINE" ] || [ -z "$JENKINS_PERSISTENT_WORKDIR" ] || [ -z "$buildFlavor" ]; then
@@ -136,9 +136,9 @@ else
 	exit 1
 fi
 
-# When supervisorTag is provided, set the appropiate barys argument
-if [ "$supervisorTag" != "__ignore__" ]; then
-	BARYS_ARGUMENTS_VAR="$BARYS_ARGUMENTS_VAR --supervisor-tag $supervisorTag"
+# When supervisorVersion is provided, set the appropiate barys argument
+if [ "$supervisorVersion" != "__ignore__" ]; then
+	BARYS_ARGUMENTS_VAR="$BARYS_ARGUMENTS_VAR --supervisor-version $supervisorVersion"
 fi
 
 # Checkout meta-balena
