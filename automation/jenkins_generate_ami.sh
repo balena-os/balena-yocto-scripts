fb1763984d76218adc2510da6cb673fbad7197be
Michal Toman <michalt@balena.io>
2021-10-20 07:06:01 +0200

    jenkins_generate_ami.sh: Preload outside of yocto cache
    Since the file name is hardcoded at this moment, this fails when two
    builds are running in parallel (e.g. dev and prod variants during deploy)
    because they try to preload the same file at pretty much the same moment.
    Having a separate copy for each outside of yocto cache should fix the issue.
    
    Change-type: patch
    Signed-off-by: Michal Toman <michalt@balena.io>

--- automation/jenkins_generate_ami.sh
+++ automation/jenkins_generate_ami.sh
@@ -42,7 +42,7 @@ YOCTO_IMAGES_PATH="${WORKSPACE}/build/tmp/deploy/images/${MACHINE}"
 IMAGE_NAME=${IMAGE_NAME:-balena-image-${MACHINE}.balenaos-img}
 
 ORIG_IMAGE="${YOCTO_IMAGES_PATH}/${IMAGE_NAME}"
-PRELOADED_IMAGE="${ORIG_IMAGE}.preloaded"
+PRELOADED_IMAGE="$(mktemp)"
 
 cp "${ORIG_IMAGE}" "${PRELOADED_IMAGE}"
 
