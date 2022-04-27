969f6b941bd9fb34d82fbc0d48838b7b4898b2e1
Alex Gonzalez <alexg@balena.io>
2022-04-27 14:48:04 +0200

    balena-lib: Extract release finalization as function
    This allows it to be called from other context, like the OS automation
    test suite.
    
    Change-type: patch
    Signed-off-by: Alex Gonzalez <alexg@balena.io>

--- automation/entry_scripts/balena-deploy-block.sh
+++ automation/entry_scripts/balena-deploy-block.sh
@@ -35,57 +35,7 @@ fi
 
 # Legacy hostapp tagging
 if [ "${DEPLOY}" = "yes" ]; then
-	_version=$(balena_api_get_version "${_releaseID}" "${API_ENV}" "${BALENAOS_TOKEN}")
-	_os_version=$(balena_lib_get_os_version)
-	# 0.0.0 is a reserved version used when the semver is not set
-	if [ "${_version%-*}" != "0.0.0" ] && [ "${_version}" != "${_os_version}" ]; then
-		echo "balena-deploy-block: Version mismatch, OS version is ${_os_version} and deployed version is ${_version}"
-		exit 1
-	fi
-	if balena_api_release_tag_exists "${BALENAOS_ACCOUNT}/$APPNAME" "version" "${_os_version}" "${API_ENV}" "${BALENAOS_TOKEN}" > /dev/null; then
-			echo "[WARN] Release ID ${_releaseID} is already tagged with version ${_os_version} - bailing out"
-			exit 0
-	fi
-	echo "[INFO] Tagging release ${_releaseID} with version ${_os_version}"
-	balena tag set version "${_os_version}" --release "${_releaseID}"
-	balena release finalize "${_releaseID}"
-	if [ "$ESR" = "true" ]; then
-		_regex="^[1-3][0-9]{3}\.${Q1ESR}|${Q2ESR}|${Q3ESR}|${Q4ESR}\.[0-9]*$"
-
-		if ! echo "${RELEASE_VERSION}" | grep -Eq "${_regex}"; then
-			>&2 echo "Invalid ESR release ${RELEASE_VERSION}"
-			exit 1
-		fi
-		balena tag set meta-balena-base "${META_BALENA_VERSION}" --release "${_releaseID}"
-
-		last_current=$(balena_api_fetch_fleet_tag "$APPNAME" "esr-current" "${API_ENV}" || true)
-		last_sunset=$(balena_api_fetch_fleet_tag "$APPNAME" "esr-sunset" "${API_ENV}" || true)
-		last_next=$(balena_api_fetch_fleet_tag "$APPNAME" "esr-next" "${API_ENV}" || true)
-		if [ "${last_current}" = "null" ]; then
-			echo "[INFO][${BALENAOS_ACCOUNT}/${APPNAME}] Tagging fleet with esr-current: ${RELEASE_VERSION}"
-			balena tag set esr-current "${RELEASE_VERSION}" --fleet "${BALENAOS_ACCOUNT}/$APPNAME"
-		elif [ "${last_sunset}" = "null" ]; then
-			if [ "${last_next}" = "null" ]; then
-				echo "[INFO][${BALENAOS_ACCOUNT}/${APPNAME}] Tagging fleet with esr-next: ${RELEASE_VERSION}"
-				balena tag set esr-next "${RELEASE_VERSION}" --fleet "${BALENAOS_ACCOUNT}/$APPNAME"
-			else
-				echo "[INFO][${BALENAOS_ACCOUNT}/${APPNAME}] Tagging fleet with esr-next: ${RELEASE_VERSION} esr-current: ${last_next} esr-sunset: ${last_current}"
-				balena tag set esr-next "${RELEASE_VERSION}" --fleet "${BALENAOS_ACCOUNT}/$APPNAME"
-				balena tag set esr-current "${last_next}" --fleet "${BALENAOS_ACCOUNT}/$APPNAME"
-				balena tag set esr-sunset "${last_current}" --fleet "${BALENAOS_ACCOUNT}/$APPNAME"
-			fi
-		else
-			if [ "${last_next}" = "null" ]; then
-				>&2 echo "Invalid fleet tags: current: ${last_current} next: ${last_next} sunset: ${last_sunset}"
-				exit 1
-			else
-				echo "[INFO][${BALENAOS_ACCOUNT}/${APPNAME}] Tagging fleet with esr-next: ${RELEASE_VERSION} esr-current: ${last_next} esr-sunset: ${last_current}"
-				balena tag set esr-next "${RELEASE_VERSION}" --fleet "${BALENAOS_ACCOUNT}/$APPNAME"
-				balena tag set esr-current "${last_next}" --fleet "${BALENAOS_ACCOUNT}/$APPNAME"
-				balena tag set esr-sunset "${last_current}" --fleet "${BALENAOS_ACCOUNT}/$APPNAME"
-			fi
-		fi
-	fi
+	balena_lib_release_finalize "${_releaseID}" "${BALENAOS_ACCOUNT}/${APPNAME}" "${API_ENV}" "${BALENAOS_TOKEN}" "${ESR}"
 fi
 
 balena_docker_stop
