c912a1400e64b34ee0ff92e50d0bcd029fdc13a5
Alex Gonzalez <alexg@balena.io>
2022-06-15 19:10:40 +0200

    balena-deploy-block.sh: Deploy using compose files
    Change-type: patch
    Signed-off-by: Alex Gonzalez <alexg@balena.io>

--- automation/entry_scripts/balena-deploy-block.sh
+++ automation/entry_scripts/balena-deploy-block.sh
@@ -12,9 +12,6 @@ balena_docker_start "/scratch/docker" "/var/run" "/var/log/docker.log"
 balena_docker_wait
 
 BALENAOS_ACCOUNT="${BALENAOS_ACCOUNT:-"balena_os"}"
-if [ -f "/host/appimage.docker" ]; then
-	_local_image=$(docker load -i /host/appimage.docker | cut -d: -f1 --complement | tr -d " " )
-fi
 
 echo "[INFO] Logging into $API_ENV as ${BALENAOS_ACCOUNT}"
 export BALENARC_BALENA_URL=${API_ENV}
@@ -25,22 +22,32 @@ if [ "$ESR" = "true" ]; then
 	APPNAME="${APPNAME}-esr"
 fi
 
+# Use /deploy folder to generate compose file to use local images that live there
+# Use a release dir to limit context
+RELEASE_DIR=$(balena_docker_create_compose_file "${MACHINE}" "${API_ENV}" "${RELEASE_VERSION}" "${BALENAOS_TOKEN}" "${BLOCKS}" "/deploy")
+if [ ! -f "${RELEASE_DIR}/docker-compose.yml" ]; then
+	echo "[ERROR] Failed to generate compose file"
+	exit 1
+fi
+
 if [ -f "/deploy/balena.yml" ]; then
-	echo -e "\nversion: $(balena_lib_get_os_version)" >> "/deploy/balena.yml"
+	cp "/deploy/balena.yml" "${RELEASE_DIR}"
+	echo -e "\nversion: $(balena_lib_get_os_version)" >> "${RELEASE_DIR}/balena.yml"
 fi
 
 echo "[INFO] Deploying  to ${BALENAOS_ACCOUNT}/$APPNAME"
 balena_api_create_public_app "${APPNAME}" "${BALENARC_BALENA_URL}" "${MACHINE}" "${balenaCloudEmail}" "${balenaCloudPassword}" "${ESR}" "${BOOTABLE}"
-_releaseID=$(balena_lib_release "${BALENAOS_ACCOUNT}/$APPNAME" "${FINAL}" "/deploy" "${API_ENV}" "$_local_image")
+_releaseID=$(balena_lib_release "${BALENAOS_ACCOUNT}/$APPNAME" "${FINAL}" "${RELEASE_DIR}" "${API_ENV}")
 if [ -z "${_releaseID}" ]; then
 	echo "[INFO] Failed to deploy to ${BALENAOS_ACCOUNT}/$APPNAME"
 	exit 1
 fi
 
 # Legacy hostapp tagging
-if [ "${DEPLOY}" = "yes" ] && [ "${FINAL}" = "yes" ]; then
+if [ "${FINAL}" = "yes" ]; then
 	balena_lib_release_finalize "${_releaseID}" "${BALENAOS_ACCOUNT}/${APPNAME}" "${API_ENV}" "${BALENAOS_TOKEN}" "${ESR}"
 fi
 
 balena_docker_stop
+rm -rf "${RELEASE_DIR:?}"
 exit 0
